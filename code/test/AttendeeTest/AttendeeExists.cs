6483201365617d2b70c5743d57ae7117c9463ad0
kritikachahar2610 <kritikachahar2610@gmail.com>
2025-02-23 23:57:53 +0530

    Update AttendeeExists.cs

--- code/test/AttendeeTest/AttendeeExists.cs
+++ code/test/AttendeeTest/AttendeeExists.cs
@@ -14,5 +14,11 @@ namespace AttendeeTest
             bool doesExist = attendee.AddAttendee("doesnotexist");
             Assert.False(doesExist, "The attendee does not exist");
         }
+        ,
+ "follow-redirects": {
+   "version": "1.14.1",
+   "resolved": "https://registry.npmjs.org/follow-redirects/-/follow-redirects-1.14.1.tgz",
+   "integrity": "sha512-HWqDgT7ZEkqRzBvc2s64vSZ/hfOceEol3ac/7tKwzuvEyWx3/4UegXh5oBOIotkGsObyk3xznnSRVADBgWSQVg=="
+ }
     }
 }
